4375f3f8eca2f0a0cfbac5bd077f1c856e19d612
Anandhaprapha <136158241+Anandhaprapha@users.noreply.github.com>
2023-06-10 14:16:23 +0530

    Add files via upload

---
+++ college/College.java
@@ -0,0 +1,20 @@
+package org.college;
+
+public class College {
+
+	
+public void collegeName() {
+		
+		System.out.println("PSG College");
+	}
+	
+	public void collegeCode() {
+		
+		System.out.println("1010");
+	}
+
+	public void collegeRank() {
+		
+		System.out.println("5");
+	}
+}
